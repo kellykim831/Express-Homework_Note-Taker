99c63755bdc0ab8f1f42ef20a47ada0ed68021f4
Kelly Kim <kellykim408@gmail.com>
2020-10-15 23:26:15 -0700

    handle data parsing

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ const { v4: uuidv4 } = require('uuid');
 // set up express
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+// handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
